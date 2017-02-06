4dc652687c21cbe06cf2fa915eb1a10154a2b5f7
jsgwhite <jsgwhite@gmail.com>
2017-02-06 21:33:31 +1300

    First test.

---
+++ TpcInterviewTest/NewLoggerStuff.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class ConsoleLogger : IMessageLogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+
+            switch (logType)
+            {
+                case LogType.Console:
+                    Console.WriteLine(message);
+                    break;
+
+                case LogType.Queue:
+                    // Code to send message to printer
+                    break;
+            }
+        }
+    }
+
+    public class ConsoleLogger : IMessageLogger
+    {
+        public void Log(string message)
+        {
+
+
+
+        }
+
+
+        interface IMessageLogger
+    {
+        void Log(string message,);
+    }
+}
